aea2b7f0eeaae939bc127268d6d74fb4926a9af0
Harsh Bajpai <bajpaiharsh244@gmail.com>
2024-07-23 20:31:07 +0800

    feat: use hardhat plugin for contract interaction

--- tasks/nil/mintShardedNFT.ts
+++ tasks/nil/mintShardedNFT.ts
@@ -1,15 +1,7 @@
 import { task } from "hardhat/config";
-import {
-  PublicClient,
-  HttpTransport,
-  LocalECDSAKeySigner,
-  type Hex,
-  WalletV1,
-  waitTillCompleted,
-} from "@nilfoundation/niljs";
+import { type Hex } from "@nilfoundation/niljs";
 // import { PublicClient, HttpTransport, LocalECDSAKeySigner, type Hex, WalletV1, waitTillCompleted } from "../../nil.js"
 import { config } from "dotenv";
-import { encodeFunctionData } from "viem";
 
 config();
 
@@ -34,76 +26,9 @@ task("mint", "Mint sharded NFT").setAction(async (taskArgs, hre) => {
     throw new Error("NFT_CONTRACT_ADDR is not set");
   }
 
-  const shardId = 1;
+  const NFTContract = await hre.ethers.getContractFactory("ShardedNFT");
+  const nftContract = NFTContract.attach(nftAddress);
 
-  const client = new PublicClient({
-    transport: new HttpTransport({
-      endpoint,
-    }),
-    shardId,
-  });
-
-  const signer = new LocalECDSAKeySigner({
-    privateKey,
-  });
-
-  const pubKey = await signer.getPublicKey();
-
-  const wallet = new WalletV1({
-    pubkey: pubKey,
-    client,
-    signer,
-    address: walletAddress,
-  });
-
-  const wa = wallet.getAddressHex();
-  console.log("wallet address: ", wa);
-
-  const artifact = await hre.artifacts.readArtifact("ShardedNFT");
-  const abi = artifact.abi;
-
-  console.log("nftAddress is", nftAddress);
-
-  const hash = await wallet.sendMessage({
-    to: nftAddress,
-    data: encodeFunctionData({
-      abi,
-      functionName: "mintTo",
-      args: [walletAddress.toLowerCase(), 26],
-    }),
-    gas: 200000n,
-    value: 5000000n,
-  });
-
-  console.log("NFT mint transaction hash: ", hash);
-
-  const result = await client.call(
-    {
-      from: walletAddress,
-      to: nftAddress,
-      data: encodeFunctionData({
-        abi,
-        functionName: "getShardID",
-        args: [26],
-      }),
-    },
-    "latest",
-  );
-
-  console.log("shard ID to be deployed:", result);
-
-  const currentShardIDResult = await client.call(
-    {
-      from: walletAddress,
-      to: nftAddress,
-      data: encodeFunctionData({
-        abi,
-        functionName: "shardId",
-        args: [],
-      }),
-    },
-    "latest",
-  );
-
-  console.log("shard ID of the current contract:", currentShardIDResult);
+  const response = await nftContract.mintTo(walletAddress.toLowerCase(), 28);
+  console.log(response);
 });
