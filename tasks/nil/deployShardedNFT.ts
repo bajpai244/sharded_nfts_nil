7ac27678157e9a4a76f2795172c5ac4c98bad86d
Harsh Bajpai <bajpaiharsh244@gmail.com>
2024-07-23 18:37:05 +0800

    dev: formatting

--- tasks/nil/deployShardedNFT.ts
+++ tasks/nil/deployShardedNFT.ts
@@ -1,70 +1,76 @@
 import { task } from "hardhat/config";
-import { PublicClient, HttpTransport, LocalECDSAKeySigner, type Hex, WalletV1, waitTillCompleted } from "@nilfoundation/niljs"
+import {
+  PublicClient,
+  HttpTransport,
+  LocalECDSAKeySigner,
+  type Hex,
+  WalletV1,
+  waitTillCompleted,
+} from "@nilfoundation/niljs";
 // import { PublicClient, HttpTransport, LocalECDSAKeySigner, type Hex, WalletV1, waitTillCompleted } from "../../nil.js"
 import { config } from "dotenv";
 
 config();
 
-task("deploy", "Deploy the Sharded NFT contract").setAction(async (taskArgs, hre) => {
-
+task("deploy", "Deploy the Sharded NFT contract").setAction(
+  async (taskArgs, hre) => {
     const privateKey = process.env.PRIVATE_KEY as Hex | undefined;
     if (!privateKey) {
-        throw new Error("PRIVATE_KEY is not set");
+      throw new Error("PRIVATE_KEY is not set");
     }
 
-    const walletAddress = process.env.WALLET_ADDR as Hex | undefined ;
+    const walletAddress = process.env.WALLET_ADDR as Hex | undefined;
     if (!walletAddress) {
-        throw new Error("WALLET_ADDR is not set");
+      throw new Error("WALLET_ADDR is not set");
     }
 
     const endpoint = process.env.NIL_RPC_ENDPOINT;
     if (!endpoint) {
-        throw new Error("NIL_RPC_ENDPOINT is not set");
+      throw new Error("NIL_RPC_ENDPOINT is not set");
     }
 
     const shardId = 1;
 
     const client = new PublicClient({
-        transport: new HttpTransport({
-          endpoint
-        }),
-        shardId
-      });
-
+      transport: new HttpTransport({
+        endpoint,
+      }),
+      shardId,
+    });
 
     const signer = new LocalECDSAKeySigner({
-        privateKey
+      privateKey,
     });
 
     const pubKey = await signer.getPublicKey();
 
     const wallet = new WalletV1({
-        pubkey: pubKey,
-        client,
-        signer,
-        address: walletAddress
-    })
+      pubkey: pubKey,
+      client,
+      signer,
+      address: walletAddress,
+    });
 
     const wa = wallet.getAddressHex();
-    console.log('wallet address: ', wa);
+    console.log("wallet address: ", wa);
 
     const artifact = await hre.artifacts.readArtifact("ShardedNFT");
     const abi = artifact.abi;
     const bytecode = artifact.bytecode as Hex;
 
-    const {address, hash}  = await wallet.deployContract({
+    const { address, hash } = await wallet.deployContract({
       bytecode,
       abi,
       salt: BigInt(Math.floor(Math.random() * 10000)),
       shardId,
-      args: [shardId,4,100],
+      args: [shardId, 4, 100],
       gas: 200000n,
-      value : 5000000n,
+      value: 5000000n,
     });
 
     await waitTillCompleted(client, 1, hash);
 
-    console.log('Contract deployed at address: ', address);
-    console.log('Transaction hash: ', hash);
-
-});
+    console.log("Contract deployed at address: ", address);
+    console.log("Transaction hash: ", hash);
+  },
+);
--- tasks/nil/mintShardedNFT.ts
+++ tasks/nil/mintShardedNFT.ts
@@ -1,5 +1,12 @@
 import { task } from "hardhat/config";
-import { PublicClient, HttpTransport, LocalECDSAKeySigner, type Hex, WalletV1, waitTillCompleted } from "@nilfoundation/niljs"
+import {
+  PublicClient,
+  HttpTransport,
+  LocalECDSAKeySigner,
+  type Hex,
+  WalletV1,
+  waitTillCompleted,
+} from "@nilfoundation/niljs";
 // import { PublicClient, HttpTransport, LocalECDSAKeySigner, type Hex, WalletV1, waitTillCompleted } from "../../nil.js"
 import { config } from "dotenv";
 import { encodeFunctionData } from "viem";
@@ -7,98 +14,96 @@ import { encodeFunctionData } from "viem";
 config();
 
 task("mint", "Mint sharded NFT").setAction(async (taskArgs, hre) => {
+  const privateKey = process.env.PRIVATE_KEY as Hex | undefined;
+  if (!privateKey) {
+    throw new Error("PRIVATE_KEY is not set");
+  }
 
-    const privateKey = process.env.PRIVATE_KEY as Hex | undefined;
-    if (!privateKey) {
-        throw new Error("PRIVATE_KEY is not set");
-    }
+  const walletAddress = process.env.WALLET_ADDR as Hex | undefined;
+  if (!walletAddress) {
+    throw new Error("WALLET_ADDR is not set");
+  }
 
-    const walletAddress = process.env.WALLET_ADDR as Hex | undefined ;
-    if (!walletAddress) {
-        throw new Error("WALLET_ADDR is not set");
-    }
+  const endpoint = process.env.NIL_RPC_ENDPOINT;
+  if (!endpoint) {
+    throw new Error("NIL_RPC_ENDPOINT is not set");
+  }
 
-    const endpoint = process.env.NIL_RPC_ENDPOINT;
-    if (!endpoint) {
-        throw new Error("NIL_RPC_ENDPOINT is not set");
-    }
+  const nftAddress = process.env.NFT_CONTRACT_ADDR as Hex | undefined;
+  if (!nftAddress) {
+    throw new Error("NFT_CONTRACT_ADDR is not set");
+  }
 
-    const nftAddress = process.env.NFT_CONTRACT_ADDR as Hex | undefined;
-    if (!nftAddress) {
-        throw new Error("NFT_CONTRACT_ADDR is not set");
-    }
+  const shardId = 1;
 
-    const shardId = 1;
+  const client = new PublicClient({
+    transport: new HttpTransport({
+      endpoint,
+    }),
+    shardId,
+  });
 
-    const client = new PublicClient({
-        transport: new HttpTransport({
-          endpoint
-        }),
-        shardId
-      });
+  const signer = new LocalECDSAKeySigner({
+    privateKey,
+  });
 
+  const pubKey = await signer.getPublicKey();
 
-    const signer = new LocalECDSAKeySigner({
-        privateKey
-    });
+  const wallet = new WalletV1({
+    pubkey: pubKey,
+    client,
+    signer,
+    address: walletAddress,
+  });
 
-    const pubKey = await signer.getPublicKey();
+  const wa = wallet.getAddressHex();
+  console.log("wallet address: ", wa);
 
-    const wallet = new WalletV1({
-        pubkey: pubKey,
-        client,
-        signer,
-        address: walletAddress
-    })
+  const artifact = await hre.artifacts.readArtifact("ShardedNFT");
+  const abi = artifact.abi;
 
-    const wa = wallet.getAddressHex();
-    console.log('wallet address: ', wa);
+  console.log("nftAddress is", nftAddress);
 
-    const artifact = await hre.artifacts.readArtifact("ShardedNFT");
-    const abi = artifact.abi;
+  const hash = await wallet.sendMessage({
+    to: nftAddress,
+    data: encodeFunctionData({
+      abi,
+      functionName: "mintTo",
+      args: [walletAddress.toLowerCase(), 26],
+    }),
+    gas: 200000n,
+    value: 5000000n,
+  });
 
-    console.log("nftAddress is", nftAddress);
+  console.log("NFT mint transaction hash: ", hash);
 
-    const hash = await wallet.sendMessage({
-        to: nftAddress,
-       data: encodeFunctionData(
-        {
-         abi,
-         functionName: "mintTo",
-        args: [walletAddress.toLowerCase(), 26],
-        }),
-        gas: 200000n,
-        value : 5000000n,
-    });
+  const result = await client.call(
+    {
+      from: walletAddress,
+      to: nftAddress,
+      data: encodeFunctionData({
+        abi,
+        functionName: "getShardID",
+        args: [26],
+      }),
+    },
+    "latest",
+  );
 
-    console.log('NFT mint transaction hash: ', hash);
+  console.log("shard ID to be deployed:", result);
 
-const result =  await client.call({
-        from: walletAddress,
-        to: nftAddress,
-        data: encodeFunctionData(
-            {
-                abi,
-                functionName: "getShardID",
-                args: [26]
-            }
-        ),
-    }, "latest");
+  const currentShardIDResult = await client.call(
+    {
+      from: walletAddress,
+      to: nftAddress,
+      data: encodeFunctionData({
+        abi,
+        functionName: "shardId",
+        args: [],
+      }),
+    },
+    "latest",
+  );
 
-    console.log("shard ID to be deployed:", result);
-
-    const currentShardIDResult =  await client.call({
-        from: walletAddress,
-        to: nftAddress,
-        data: encodeFunctionData(
-            {
-                abi,
-                functionName: "shardId",
-                args: []
-            }
-        ),
-    }, "latest");
-
-    console.log("shard ID of the current contract:", currentShardIDResult);
-
+  console.log("shard ID of the current contract:", currentShardIDResult);
 });
